b3e2984fad9a47d3787b8331d5cf7b337f84b576
Ruan Cardoso dos Santos <cardosoruan2001@gmail.com>
2023-09-16 00:34:59 -0300

    style: Ajustes nos cards da main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Home as HomeIcon, Search, Library, PlusSquare, Heart, ChevronLeft, ChevronRight, ChevronDown } from 'lucide-react'
+import { Home as HomeIcon, Search, Library, PlusSquare, Heart, ChevronLeft, ChevronRight, ChevronDown, Play } from 'lucide-react'
 
 import Image from 'next/image'
 
@@ -60,7 +60,7 @@ export default function Home() {
 
         </aside>
 
-        <main className="flex-1 p-6">
+        <main className="flex-1 p-6 mb-14 overflow-auto">
           <div className='flex items-center justify-between'>
 
             <div className="flex items-center gap-5">
@@ -87,62 +87,147 @@ export default function Home() {
 
           <h1 className='text-3xl font-semibold mt-10'>Good Afternoon</h1>
 
-          <div className='grid grid-cols-3 gap-5 mt-5'>
-            <div className='flex items-center bg-white/20 rounded overflow-hidden'>
-              <Image src="/astroworld.jpg" width={100} height={100} alt="ASTROWORLD Album Cover" />
-              <h1 className='ml-4 font-semibold'>ASTROWORLD</h1>
-            </div>
-            <div className='flex items-center bg-white/20 rounded overflow-hidden'>
-              <Image src="/issa.png" width={100} height={100} alt='ISSA Album Cover' />
+          <div className='grid grid-cols-3 gap-5 mt-8'>
+            <a href='' className='flex items-center justify-between bg-white/20 rounded overflow-hidden'>
+              <div className='flex items-center'>
+                <div className='overflow-hidden'>
+                  <Image src="/astroworld.jpg" width={100} height={100} alt="ASTROWORLD Album Cover" className='hover:scale-125 transition-all duration-500' />
+                </div>
+                <h1 className='ml-4 font-semibold'>ASTROWORLD</h1>
+              </div>
+              <button className='bg-green-600 rounded-full p-3 opacity-0 hover:opacity-90 transition-opacity duration-300 '>
+                <Play />
+              </button> {/*rever como ajeitar esse botao no final da box (tentar com position)*/}
+            </a>
+            <a href='' className='flex items-center bg-white/20 rounded overflow-hidden'>
+              <div className='overflow-hidden'>
+                <Image src="/issa.png" width={100} height={100} alt='ISSA Album Cover' className='hover:scale-125 transition-all duration-500' />
+              </div>
               <h1 className='ml-4 font-semibold'>ISSA Album</h1>
-            </div>
-            <div className='flex items-center bg-white/20 rounded overflow-hidden'>
-              <Image src="/maquinadotempo.webp" width={100} height={100} alt='Máquina do Tempo Album Cover' />
+              <button className='ml-24 bg-green-600 rounded-full p-3 opacity-0 hover:opacity-90 transition-opacity duration-300 '>
+                <Play />
+              </button>
+            </a>
+            <a href='' className='flex items-center bg-white/20 rounded overflow-hidden'>
+              <div className="overflow-hidden">
+                <Image src="/maquinadotempo.webp" width={100} height={100} alt='Máquina do Tempo Album Cover' className='hover:scale-125 transition-all duration-500' />
+              </div>
               <h1 className='ml-4 font-semibold'>Máquina do Tempo</h1>
-            </div>
-            <div className='flex items-center bg-white/20 rounded overflow-hidden'>
-              <Image src="/savagemode.jpg" width={100} height={100} alt='Savage Mode II Album Cover' />
+              <button className='ml-10 bg-green-600 rounded-full p-3 opacity-0 hover:opacity-90 transition-opacity duration-300 '>
+                <Play />
+              </button>
+            </a >
+            <a href='' className='flex items-center bg-white/20 rounded overflow-hidden'>
+              <div className="overflow-hidden">
+                <Image src="/savagemode.jpg" width={100} height={100} alt='Savage Mode II Album Cover' className='hover:scale-125 transition-all duration-500' />
+              </div>
               <h1 className='ml-4 font-semibold'>SAVAGE MODE II</h1>
-            </div>
-            <div className='flex items-center bg-white/20 rounded overflow-hidden'>
-              <Image src="/We_Love_You_Tecca.jpeg" height={100} width={100} alt='We Love You Tecca Album Cover' />
+              <button className='ml-16 bg-green-600 rounded-full p-3 opacity-0 hover:opacity-90 transition-opacity duration-300 '>
+                <Play />
+              </button>
+            </a>
+            <a href='' className='flex items-center bg-white/20 rounded overflow-hidden'>
+              <div className="overflow-hidden">
+                <Image src="/We_Love_You_Tecca.jpeg" height={100} width={100} alt='We Love You Tecca Album Cover' className='hover:scale-125 transition-all duration-500' />
+              </div>
               <h1 className='ml-4 font-semibold'>We Love You Tecca</h1>
-            </div>
-            <div className='flex items-center bg-white/20 rounded overflow-hidden'>
-              <Image src="/utopia.png" width={100} height={100} alt='UTOPIA Album Cover' />
+              <button className='ml-12 bg-green-600 rounded-full p-3 opacity-0 hover:opacity-90 transition-opacity duration-300 '>
+                <Play />
+              </button>
+            </a>
+            <a href='' className='flex items-center bg-white/20 rounded overflow-hidden'>
+              <div className="overflow-hidden">
+                <Image src="/utopia.png" width={100} height={100} alt='UTOPIA Album Cover' className='hover:scale-125 transition-all duration-500' />
+              </div>
               <h1 className='ml-4 font-semibold'>UTOPIA</h1>
-            </div>
+              <button className='ml-32 bg-green-600 rounded-full p-3 opacity-0 hover:opacity-90 transition-opacity duration-300 '>
+                <Play />
+              </button>
+            </a>
           </div>
 
-          <h1 className='text-3xl font-semibold mt-5 mb-5'>Made For Ruan Cardoso</h1>
+          <div className="flex justify-between items-center">
+            <h1 className='text-3xl font-semibold mt-8 mb-8'>Made For Ruan Cardoso</h1>
+            <a href="" className='text-zinc-400 opacity-80 text-lg font-semibold hover:text-zinc-50 hover:opacity-100 hover:text-xl transition-all duration-300'>Show all</a>
+          </div>
 
-          <div className='flex items-center justify-center gap-20 px-2'>
+          <div className='h-96 grid grid-cols-5 gap-16 px-2 overflow-y-scroll'>
 
-            <div>
+            <a className='bg-zinc-800 p-3 rounded'>
               <Image src="/GiftCourse.png" width={290} height={290} alt='Gift and Course Album Cover' className='rounded'/>
-              <h1 className='mt-3 mb-1 font-semibold'>Gift and Course</h1>
+              <h1 className='mt-5 mb-1 font-semibold'>Gift & a Curse</h1>
               <p className='text-zinc-400 opacity-90'>Gunna</p>
-            </div>
-            <div>
+            </a>
+            <a className='bg-zinc-800 p-3 rounded'>
               <Image src="/meetthewoo.jpeg" width={290} height={290} alt='Meet the WOO Album Cover' className='rounded'/>
-              <h1 className='mt-3 mb-1 font-semibold'>Meet the Woo</h1>
+              <h1 className='mt-5 mb-1 font-semibold'>Meet the Woo</h1>
               <p className='text-zinc-400 opacity-90'>Pop Smoke</p>
-            </div>
-            <div>
+            </a>
+            <a className='bg-zinc-800 p-3 rounded'>
               <Image src="/meetthewoo2.jpg" width={290} height={290} alt='Gift and Course Album Cover' className='rounded'/>
-              <h1 className='mt-3 mb-1 font-semibold'>Meet the Woo 2</h1>
+              <h1 className='mt-5 mb-1 font-semibold'>Meet the Woo 2</h1>
               <p className='text-zinc-400 opacity-90'>Pop Smoke</p>
-            </div>
-            <div>
+            </a>
+            <a className='bg-zinc-800 p-3 rounded'>
               <Image src="/tourlife3.jpeg" width={290} height={290} alt='Gift and Course Album Cover' className='rounded'/>
-              <h1 className='mt-3 mb-1 font-semibold'>XO Tour Llife3</h1>
+              <h1 className='mt-5 mb-1 font-semibold'>XO Tour Llife3</h1>
               <p className='text-zinc-400 opacity-90'>Lil Uzi Vert</p>
-            </div>
-            <div>
+            </a>
+            <a className='bg-zinc-800 p-3 rounded'>
               <Image src="/mellyvsmelvin.png" width={290} height={290} alt='Gift and Course Album Cover' className='rounded'/>
-              <h1 className='mt-3 mb-1 font-semibold'>Melly vs Melvin</h1>
+              <h1 className='mt-5 mb-1 font-semibold'>Melly vs Melvin</h1>
               <p className='text-zinc-400 opacity-90'>YNW Melly</p>
-            </div>
+            </a>
+            <a className='bg-zinc-800 p-3 rounded'>
+              <Image src="/GiftCourse.png" width={290} height={290} alt='Gift and Course Album Cover' className='rounded'/>
+              <h1 className='mt-5 mb-1 font-semibold'>Gift & a Curse</h1>
+              <p className='text-zinc-400 opacity-90'>Gunna</p>
+            </a>
+            <a className='bg-zinc-800 p-3 rounded'>
+              <Image src="/meetthewoo.jpeg" width={290} height={290} alt='Meet the WOO Album Cover' className='rounded'/>
+              <h1 className='mt-5 mb-1 font-semibold'>Meet the Woo</h1>
+              <p className='text-zinc-400 opacity-90'>Pop Smoke</p>
+            </a>
+            <a className='bg-zinc-800 p-3 rounded'>
+              <Image src="/meetthewoo2.jpg" width={290} height={290} alt='Gift and Course Album Cover' className='rounded'/>
+              <h1 className='mt-5 mb-1 font-semibold'>Meet the Woo 2</h1>
+              <p className='text-zinc-400 opacity-90'>Pop Smoke</p>
+            </a>
+            <a className='bg-zinc-800 p-3 rounded'>
+              <Image src="/tourlife3.jpeg" width={290} height={290} alt='Gift and Course Album Cover' className='rounded'/>
+              <h1 className='mt-5 mb-1 font-semibold'>XO Tour Llife3</h1>
+              <p className='text-zinc-400 opacity-90'>Lil Uzi Vert</p>
+            </a>
+            <a className='bg-zinc-800 p-3 rounded'>
+              <Image src="/mellyvsmelvin.png" width={290} height={290} alt='Gift and Course Album Cover' className='rounded'/>
+              <h1 className='mt-5 mb-1 font-semibold'>Melly vs Melvin</h1>
+              <p className='text-zinc-400 opacity-90'>YNW Melly</p>
+            </a>
+            <a className='bg-zinc-800 p-3 rounded'>
+              <Image src="/GiftCourse.png" width={290} height={290} alt='Gift and Course Album Cover' className='rounded'/>
+              <h1 className='mt-5 mb-1 font-semibold'>Gift & a Curse</h1>
+              <p className='text-zinc-400 opacity-90'>Gunna</p>
+            </a>
+            <a className='bg-zinc-800 p-3 rounded'>
+              <Image src="/meetthewoo.jpeg" width={290} height={290} alt='Meet the WOO Album Cover' className='rounded'/>
+              <h1 className='mt-5 mb-1 font-semibold'>Meet the Woo</h1>
+              <p className='text-zinc-400 opacity-90'>Pop Smoke</p>
+            </a>
+            <a className='bg-zinc-800 p-3 rounded'>
+              <Image src="/meetthewoo2.jpg" width={290} height={290} alt='Gift and Course Album Cover' className='rounded'/>
+              <h1 className='mt-5 mb-1 font-semibold'>Meet the Woo 2</h1>
+              <p className='text-zinc-400 opacity-90'>Pop Smoke</p>
+            </a>
+            <a className='bg-zinc-800 p-3 rounded'>
+              <Image src="/tourlife3.jpeg" width={290} height={290} alt='Gift and Course Album Cover' className='rounded'/>
+              <h1 className='mt-5 mb-1 font-semibold'>XO Tour Llife3</h1>
+              <p className='text-zinc-400 opacity-90'>Lil Uzi Vert</p>
+            </a>
+            <a className='bg-zinc-800 p-3 rounded'>
+              <Image src="/mellyvsmelvin.png" width={290} height={290} alt='Gift and Course Album Cover' className='rounded'/>
+              <h1 className='mt-5 mb-1 font-semibold'>Melly vs Melvin</h1>
+              <p className='text-zinc-400 opacity-90'>YNW Melly</p>
+            </a>
 
           </div>
 
@@ -150,7 +235,7 @@ export default function Home() {
 
       </div>
 
-      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
+      <footer className="bg-zinc-800 border-t border-zinc-700 p-6 fixed bottom-0 w-screen">
         Footer
       </footer>
 
